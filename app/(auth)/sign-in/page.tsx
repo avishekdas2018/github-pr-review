ed28bbefb0b2c40ca12b7329021e4814f3b4b263
avishekdas2018 <dasavishek2013@gmail.com>
2026-06-24 10:44:35 +0530

    feat: enhance sign-in page with structured layout and GitHub sign-in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import type { Metadata } from "next";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldSet,
+} from "@/components/ui/field";
 
-const SignInPage = () => {
+export const metadata: Metadata = {
+  title: "Sign in",
+  description: "Sing in to GitHub Code Reviewer with your github account",
+};
+
+type SignInPageProps = {
+  searchParams: Promise<{ callbackUrl?: string }>;
+};
+
+const SignInPage = async ({ searchParams }: SignInPageProps) => {
   return (
-    <div>SignInPage</div>
-  )
-}
+    <Card className="border-border/80 shadow-sm">
+      <CardHeader className="items-center text-center">
+        <div className="mb-6 justify-center pt-2">
+          <Image
+            src="/productlogo.svg"
+            alt="Product mainlogo"
+            width={100}
+            height={100}
+            priority
+            className="text-foreground"
+          />
+        </div>
+        <CardTitle className="text-base">Welcome back!</CardTitle>
+        <CardDescription>
+          Sing in with GitHub to review and manage your code
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <FieldSet>
+          <FieldGroup>
+            <Field>
+              <GitHubSignInForm callbackUrl={callbackUrl} />
+              <FieldDescription>
+                We only request the permission we needed to identify your
+                account. You can revoke access anytime from GitHub settings.
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default SignInPage
+export default SignInPage;
